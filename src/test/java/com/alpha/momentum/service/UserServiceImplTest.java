ea64c0c7f54fea9f8007412744b33ef0cb1b558f
Dhruva Raju <49dhruva@gmail.com>
2022-08-11 20:37:00 +0530

    feat(unit-tests): added for user service

--- src/test/java/com/alpha/momentum/service/UserServiceImplTest.java
+++ src/test/java/com/alpha/momentum/service/UserServiceImplTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -62,7 +63,7 @@ class UserServiceImplTest {
 
     @Tag("happy-path")
     @Tag("create")
-    @DisplayName("Should add a new user when valid request is provided")
+    @DisplayName("01 - Should add a new user when valid request is provided")
     @Test
     void addNewUser() {
         when(repository.save(any())).thenReturn(entity);
@@ -77,19 +78,99 @@ class UserServiceImplTest {
         verify(repository, times(1)).save(any());
     }
 
+    @Tag("happy-path")
+    @Tag("read")
+    @DisplayName("02 - Should fetch all users")
     @Test
     void retrieveAllUsers() {
+        when(repository.findAll()).thenReturn(userEntityList);
+        List<UserResponse> result = service.retrieveAllUsers();
+        assertAll("Fetch all users Check",
+                () -> assertEquals(ID, result.get(0).getId()),
+                () -> assertEquals(USERNAME, result.get(0).getUserName()),
+                () -> assertEquals(FIRSTNAME, result.get(0).getFirstName()),
+                () -> assertEquals(LASTNAME, result.get(0).getLastName()),
+                () -> assertEquals(EMAIL, result.get(0).getEmail()),
+                () -> assertEquals(ROLE, result.get(0).getRole()));
+        verify(repository, times(1)).findAll();
     }
 
+    @Tag("happy-path")
+    @Tag("read")
+    @DisplayName("03 - Should fetch empty list when no users are present")
+    @Test
+    void retrieveAllUsersReturnsEmpty() {
+        when(repository.findAll()).thenReturn(new ArrayList<>());
+        List<UserResponse> result = service.retrieveAllUsers();
+        assertAll("Fetch all users Check",
+                () -> assertEquals(0, result.size()));
+        verify(repository, times(1)).findAll();
+    }
+
+    @Tag("happy-path")
+    @Tag("read")
+    @DisplayName("04 - Should retrieve user based on Id")
     @Test
     void retrieveUserById() {
+        when(repository.findById(ID)).thenReturn(Optional.of(entity));
+        UserResponse result = service.retrieveUserById(ID);
+        assertAll("User Entity By Id",
+                () -> assertEquals(ID, result.getId()),
+                () -> assertEquals(USERNAME, result.getUserName()),
+                () -> assertEquals(FIRSTNAME, result.getFirstName()),
+                () -> assertEquals(LASTNAME, result.getLastName()),
+                () -> assertEquals(EMAIL, result.getEmail()),
+                () -> assertEquals(ROLE, result.getRole()));
+        verify(repository, times(1)).findById(ID);
     }
 
+    @Tag("happy-path")
+    @Tag("read")
+    @DisplayName("05 - Should return null when user not available with provided Id")
+    @Test
+    void retrieveUserByIdReturnsNull() {
+        when(repository.findById(ID)).thenReturn(Optional.empty());
+        UserResponse result = service.retrieveUserById(ID);
+        assertNull(result);
+        verify(repository, times(1)).findById(ID);
+    }
+
+    @Tag("happy-path")
+    @Tag("create")
+    @DisplayName("06 - Should save an updated user")
     @Test
     void updateUserById() {
+        when(repository.findById(ID)).thenReturn(Optional.of(entity));
+        when(repository.save(any())).thenReturn(entity);
+        UserResponse result = service.updateUserById(ID, request);
+        assertAll("Created Entity Check",
+                () -> assertEquals(ID, result.getId()),
+                () -> assertEquals(USERNAME, result.getUserName()),
+                () -> assertEquals(FIRSTNAME, result.getFirstName()),
+                () -> assertEquals(LASTNAME, result.getLastName()),
+                () -> assertEquals(EMAIL, result.getEmail()),
+                () -> assertEquals(ROLE, result.getRole()));
+        verify(repository, times(1)).save(any());
+        verify(repository, times(1)).findById(ID);
     }
 
+    @Tag("happy-path")
+    @Tag("create")
+    @DisplayName("07 - Should return null when trying to update an unavailable user")
+    @Test
+    void updateUserByIdReturnsNull() {
+        when(repository.findById(ID)).thenReturn(Optional.empty());
+        UserResponse result = service.updateUserById(ID, request);
+        assertNull(result);
+        verify(repository, times(1)).findById(ID);
+    }
+
+    @Tag("happy-path")
+    @Tag("delete")
+    @DisplayName("08 - Should delete a user")
     @Test
     void deleteUser() {
+        service.deleteUser(ID);
+        verify(repository, times(1)).deleteById(ID);
     }
 }
